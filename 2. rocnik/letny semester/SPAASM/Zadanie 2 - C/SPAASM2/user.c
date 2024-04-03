7d6df92dcb0eb851b14294a3594689ea6abd301a
GeTo0 <dominik.zatovic@gmail.com>
2024-04-03 10:34:08 +0200

    Uprava programu, nefunkcny halt

--- 2. rocnik/letny semester/SPAASM/Zadanie 2 - C/SPAASM2/user.c
+++ 2. rocnik/letny semester/SPAASM/Zadanie 2 - C/SPAASM2/user.c
@@ -53,6 +53,93 @@ int send_message(int sockfd, char *message) {
     }
 }
 
+char *lsh_read_line(void) {
+    char *line = malloc(MAX_LINE_LENGTH * sizeof(char));
+    if (!line) {
+        fprintf(stderr, "lsh: allocation error\n");
+        exit(EXIT_FAILURE);
+    }
+
+    if (!fgets(line, MAX_LINE_LENGTH, stdin)) {
+        free(line);
+        if (!feof(stdin)) {
+            perror("lsh: fgets");
+        }
+        exit(EXIT_FAILURE);
+    }
+
+    return line;
+}
+
+char **lsh_split_line(char *line) {
+    int bufsize = MAX_LINE_LENGTH, position = 0;
+    char **tokens = malloc(bufsize * sizeof(char *));
+    char *token, *saveptr;
+
+    if (!tokens) {
+        fprintf(stderr, "lsh: allocation error\n");
+        exit(EXIT_FAILURE);
+    }
+
+    // Split the line based on semicolon
+    token = strtok_r(line, ";\n", &saveptr);
+    while (token != NULL) {
+
+        // Add the token to the list
+        tokens[position] = token;
+        position++;
+
+        // Resize the buffer if necessary
+        if (position >= bufsize) {
+            bufsize += MAX_LINE_LENGTH;
+            tokens = realloc(tokens, bufsize * sizeof(char *));
+            if (!tokens) {
+                fprintf(stderr, "lsh: allocation error\n");
+                exit(EXIT_FAILURE);
+            }
+        }
+
+        // Get the next token
+        token = strtok_r(NULL, ";\n", &saveptr);
+    }
+    tokens[position] = NULL; // Null-terminate the list
+    return tokens;
+}
+
+char **lsh_split_args(char *argument) {
+    int bufsize = MAX_LINE_LENGTH, position = 0;
+    char **subargs = malloc(bufsize * sizeof(char *));
+    char *subarg;
+
+    if (!subargs) {
+        fprintf(stderr, "lsh: allocation error\n");
+        exit(EXIT_FAILURE);
+    }
+
+    // Split the line based on semicolon
+    subarg = strtok(argument, " \t\n");
+    while (subarg != NULL) {
+        // Add the token to the list
+        subargs[position] = subarg;
+        position++;
+
+        // Resize the buffer if necessary
+        if (position >= bufsize) {
+            bufsize += MAX_LINE_LENGTH;
+            subargs = realloc(subargs, bufsize * sizeof(char *));
+            if (!subargs) {
+                fprintf(stderr, "lsh: allocation error\n");
+                exit(EXIT_FAILURE);
+            }
+        }
+
+        // Get the next token
+        subarg = strtok(NULL, " \t\n");
+    }
+    subargs[position] = NULL; // Null-terminate the list
+    return subargs;
+}
+
 int lsh_execute_external(char **args) {
     pid_t pid, wpid;
     int status;
@@ -77,146 +164,90 @@ int lsh_execute_external(char **args) {
     return 1;
 }
 
-char *lsh_read_line(void) {
-    char *line = malloc(MAX_LINE_LENGTH * sizeof(char));
-    if (!line) {
-        fprintf(stderr, "lsh: allocation error\n");
-        exit(EXIT_FAILURE);
-    }
-
-    if (!fgets(line, MAX_LINE_LENGTH, stdin)) {
-        free(line);
-        if (!feof(stdin)) {
-            perror("lsh: fgets");
-        }
-        exit(EXIT_FAILURE);
-    }
-
-    return line;
-}
-
-char **lsh_split_line(char *line) {
-    int bufsize = MAX_LINE_LENGTH, position = 0;
-    char **tokens = malloc(bufsize * sizeof(char *));
-    char *token;
-
-    if (!tokens) {
-        fprintf(stderr, "lsh: allocation error\n");
-        exit(EXIT_FAILURE);
-    }
-
-    token = strtok(line, " \t\n");
-    while (token != NULL) {
-        tokens[position] = token;
-        position++;
-
-        if (position >= bufsize) {
-            bufsize += MAX_LINE_LENGTH;
-            tokens = realloc(tokens, bufsize * sizeof(char *));
-            if (!tokens) {
-                fprintf(stderr, "lsh: allocation error\n");
-                exit(EXIT_FAILURE);
-            }
-        }
-
-        token = strtok(NULL, " \t\n");
-    }
-    tokens[position] = NULL;
-    return tokens;
-}
-
 int lsh_execute(char **args, char **port, int *sockfd) {
     int i = 0;
-    /*While loop to iterate through all args*/
     while (args[i] != NULL) {
         if (strcmp(args[i], "quit") == 0) {
             if (*sockfd != -1) {
-                close(*sockfd); // Close the socket if it's open
+                close(*sockfd);
             }
             return 0;
-        }
-        else if (strcmp(args[i], "prompt")==0){
-            if (args[i+1]!=NULL){
-                set_custom_prompt(args[i+1]);
-            }
-            else{
+        } else if (strcmp(args[i], "prompt") == 0) {
+            if (args[i + 1] != NULL) {
+                set_custom_prompt(args[i + 1]);
+            } else {
                 printf("Usage: prompt <custom_prompt>\n");
             }
-        }
-        else if (strcmp(args[i], "fork") == 0) {
-            // Example of using the fork command
+        } else if (strcmp(args[i], "help") == 0) {
+            if (*sockfd != -1) {
+                char mess[MAX_LINE_LENGTH];
+                strcpy(mess, args[i]);
+                strcat(mess, " ");
+                send_message(*sockfd, mess);
+                char message[MAX_PROMPT_LENGTH];
+                memset(message, 0, sizeof(message));
+                ssize_t num_bytes = recv(*sockfd, message, sizeof(message), 0);
+                if (num_bytes > 0) {
+                    message[num_bytes] = '\0';
+                    printf("%s\n", message);
+                }
+            } else {
+                fprintf(stderr, "No connection to server\n");
+                return 1;
+            }
+        } else if (strcmp(args[i], "fork") == 0) {
             pid_t pid = fork();
             if (pid == 0) {
-                // Child process
                 printf("This is the child process.\n");
                 exit(EXIT_SUCCESS);
             } else if (pid > 0) {
-                // Parent process
                 printf("This is the parent process.\n");
             } else {
-                // Error forking
                 perror("lsh");
             }
-        }
-        else {
-            if (strcmp(args[i], "-p") == 0) {
-                if (args[i + 1] != NULL) {
-                    char *new_port = realloc(*port, (strlen(args[i + 1]) + 1) * sizeof(char));
-                    *port = new_port;
-                    strcpy(*port, args[i + 1]);
-                    if (*port == NULL) {
-                        fprintf(stderr, "Memory allocation failed\n");
-                        exit(EXIT_FAILURE);
-                    } else {
+        } else if (strstr(args[i], "-p") != NULL) {
+            char **subargs = lsh_split_args(args[i]);
+            int j = 0;
+            while (subargs[j] != NULL) {
+                if (strcmp(subargs[j], "-p") == 0) {
+                    if (subargs[j + 1] != NULL) {
+                        char *new_port = realloc(*port, (strlen(subargs[j + 1]) + 1) * sizeof(char));
+                        if (new_port == NULL) {
+                            fprintf(stderr, "Memory allocation failed\n");
+                            exit(EXIT_FAILURE);
+                        }
+                        *port = new_port;
+                        strcpy(*port, subargs[j + 1]);
                         if (*sockfd != -1) {
-                            close(*sockfd); // Close the socket if it's open
+                            close(*sockfd);
                         }
                         *sockfd = connect_to_server(port);
-                        i++;
+                    } else {
+                        fprintf(stderr, "Missing port number after -p option\n");
+                        return 1;
                     }
-                } else {
-                    fprintf(stderr, "Missing port number after -p option\n");
-                    return 1;
                 }
-            } else {
-                if (*sockfd != -1) {
-                    char mess[MAX_LINE_LENGTH];
-                    strcpy(mess, args[i]);
-                    strcat(mess, " ");
-                    send_message(*sockfd, mess);
-
-                    //Listen for incoming message from server
-                    char message[MAX_PROMPT_LENGTH];
-                    memset(message, 0, sizeof(message));
-                    ssize_t num_bytes = recv(*sockfd, message, sizeof(message), 0);
-                    if (num_bytes > 0) {
-                        message[num_bytes] = '\0';
-                        if (strcmp(message, "halt") == 0) {
-                            printf("Server sent halt command. Disconnecting...\n");
-                            close(*sockfd);
-                            *sockfd = -1;
-                            halt_flag=1;
-                            break;
-                        }
-                        else{
-                            printf("%s\n", message);
-                        }
-                    }
-                } else {
-                    //fprintf(stderr, "No connection to server\n");
-                    //return 1;
-                    return lsh_execute_external(args);
-                }
+                j++;
             }
+            free(subargs);
+        } else {
+            char **cmd_args = lsh_split_args(args[i]);
+            // Execute the command with its arguments
+            lsh_execute_external(cmd_args);
+            free(cmd_args); // Free the memory allocated for arguments
+            return 1;
         }
-        i++;
-    }
-    return 1;
+    i++;
 }
+
+return 1;
+}
+
 
 void client_side(char **port, int *sockfd) {
     char *line;
     char **args;
+    char **testing;
     int status;
     char *prompt = NULL;
     //printf("PORT IS %s\n", *port);
@@ -228,13 +259,9 @@ void client_side(char **port, int *sockfd) {
         line = lsh_read_line();
         args = lsh_split_line(line);
         status = lsh_execute(args, port, sockfd);
+
         if (halt_flag) break;
 
-
-        /* FREE MEMORY*/
-        /*for (int i = 0; args[i] != NULL; i++) {
-            free(args[i]);
-        }*/
         free(line);
         free(args);
     } while (status);
